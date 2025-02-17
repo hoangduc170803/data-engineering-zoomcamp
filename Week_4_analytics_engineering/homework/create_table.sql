a3931e7f12c054c69bafe75bba34ebaa09b73f26
hoangduc170803 <101920739+hoangduc170803@users.noreply.github.com>
2025-02-17 18:32:05 +0700

    Update create_table.sql

--- Week_4_analytics_engineering/homework/create_table.sql
+++ Week_4_analytics_engineering/homework/create_table.sql
@@ -1,1 +1,12 @@
 
+#Create External_Table
+CREATE OR REPLACE EXTERNAL TABLE `terraform-demo-447612.taxi_data.external_fhv_data_2019`
+OPTIONS (
+  format = 'PARQUET',
+  uris = ['gs://nyc_taxi_data__parquet_bucket/raw/output_fhv_trip_2019-*.parquet']
+);
+
+CREATE OR REPLACE TABLE `terraform-demo-447612.taxi_data.fhv_data_2019`
+AS
+SELECT *
+FROM `terraform-demo-447612.taxi_data.external_fhv_data_2019`;
