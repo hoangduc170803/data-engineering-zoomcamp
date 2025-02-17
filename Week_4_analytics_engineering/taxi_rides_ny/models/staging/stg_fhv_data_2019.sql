c18257985015ad7e7dcd03eedb718c672fd28e8e
Đức Nhâm <hoangduc170803@gmail.com>
2025-02-17 13:03:01 +0000

    oke

---
+++ Week_4_analytics_engineering/taxi_rides_ny/models/staging/stg_fhv_data_2019.sql
@@ -0,0 +1,25 @@
+--Homework
+with 
+
+source as (
+
+    select * from {{ source('staging', 'fhv_data_2019') }}
+
+),
+
+renamed as (
+
+    select
+        dispatching_base_num,
+        pickup_datetime,
+        dropoff_datetime,
+        pulocationid,
+        dolocationid,
+        sr_flag,
+        affiliated_base_number
+
+    from source
+
+)
+
+select * from renamed
