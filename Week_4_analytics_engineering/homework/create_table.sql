41064802ae1cb17a49a7018d0c3b69827263c4c9
hoangduc170803 <101920739+hoangduc170803@users.noreply.github.com>
2025-02-18 00:12:42 +0700

    Update create_table.sql

--- Week_4_analytics_engineering/homework/create_table.sql
+++ Week_4_analytics_engineering/homework/create_table.sql
@@ -22,3 +22,14 @@ AS
 SELECT *
 FROM `terraform-demo-447612.taxi_data.external_green_taxi_2019_2020`;
 
+#Create External_Table yellow_taxi_2019_2020
+CREATE OR REPLACE EXTERNAL TABLE `terraform-demo-447612.taxi_data.external_yellow_taxi_data_2019_2020`
+OPTIONS (
+  format = 'PARQUET',
+  uris = ['gs://nyc_taxi_data__parquet_bucket/raw/output_yellow_taxi2019-*.parquet','gs://nyc_taxi_data__parquet_bucket/raw/output_yellow_taxi2020-*.parquet']
+);
+
+CREATE OR REPLACE TABLE `terraform-demo-447612.taxi_data.yellow_taxi_data_2019_2020`
+AS
+SELECT *
+FROM `terraform-demo-447612.taxi_data.external_yellow_taxi_data_2019_2020`;
