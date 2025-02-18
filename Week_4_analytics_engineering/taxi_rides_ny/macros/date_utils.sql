2dedbcc793edc22a7e815afea8880ae13578a169
Đức Nhâm <hoangduc170803@gmail.com>
2025-02-18 14:20:00 +0000

    fix bug

--- Week_4_analytics_engineering/taxi_rides_ny/macros/date_utils.sql
+++ Week_4_analytics_engineering/taxi_rides_ny/macros/date_utils.sql
@@ -1,3 +1,3 @@
-{% macro convert_to_quarter(date_column) %}
+{% macro convert_to_quarter(date_column) -%}
     CAST(EXTRACT(YEAR FROM {{ date_column }}) AS STRING) || ' Q' || CAST(EXTRACT(QUARTER FROM {{ date_column }}) AS STRING)
 {% endmacro %}
