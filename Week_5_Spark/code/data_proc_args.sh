35eacb294dada0b020836f7011da0605e1e22f59
hoangduc170803 <hoangduc170803@gmail.com>
2025-03-02 02:43:58 +0700

    cjob submit dÃataproc

--- Week_5_Spark/code/data_proc_args.sh
+++ Week_5_Spark/code/data_proc_args.sh
@@ -8,3 +8,13 @@ gs://dtc_data_lake_de-zoomcamp_ny-taxi/code/10_spark_standalone.py
 
 
 
+# job submit
+$ gcloud dataproc jobs submit pyspark \
+    --project=terraform-demo-447612 \
+    --cluster=datazoomcamp-cluster \
+    --region=asia-southeast1 \
+    gs://dtc_data_lake_de-zoomcamp_ny-taxi/code/10_spark_standalone.py \
+    -- \
+        --input_green=gs://dtc_data_lake_de-zoomcamp_ny-taxi/pq/green/2020/*/ \
+        --input_yellow=gs://dtc_data_lake_de-zoomcamp_ny-taxi/pq/yellow/2020/*/ \
+        --output=gs://dtc_data_lake_de-zoomcamp_ny-taxi/report/report-2020
